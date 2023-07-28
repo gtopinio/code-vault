24056e8e572ead0b684f490d812ff24b9f95a665
gtopinio <mctopinio@up.edu.ph>
2023-07-28 14:00:13 +0800

    update: initialize globals.css, tailwind.config.js, and assets folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        body: ['Sora'],
+      },
+      colors: {
+        'mirage-gray': '#1e1e20',
+        'smokey-gray': '#3a3a3a',
       },
     },
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
