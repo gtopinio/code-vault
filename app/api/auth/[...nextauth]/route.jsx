7ad7a6ab0bb0a6038ee6f890c34a44b4dcba1864
gtopinio <mctopinio@up.edu.ph>
2023-07-29 01:33:36 +0800

    feature: implement route.js, initialize mongoose model, setup env vars

--- app/api/auth/[...nextauth]/route.jsx
+++ app/api/auth/[...nextauth]/route.jsx
@@ -1,18 +1,43 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: '',
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
     async session({ session }) {
-
+        const sessionUser = await User.findOne({ email: session.user.email });
+        session.user.id = sessionUser._id.toString();
+        return session; // making sure that we always know which user is logged in
     },
     async signIn({ profile }){
+        try {
+            // serverless -> lambda -> opened only when called
+            // as oppose to express -> server -> always opened
 
+            await connectToDB();
+            // check if user exists
+            const userExists = await User.findOne({ email: profile.email });
+
+            // if not, create user
+            if (!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.image,
+                })
+            }
+
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
     }
 });
 
