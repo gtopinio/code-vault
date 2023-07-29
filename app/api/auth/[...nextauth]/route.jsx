5f0f0cc00333e69d2df13d9ded187737eeee7523
gtopinio <mctopinio@up.edu.ph>
2023-07-29 14:46:44 +0800

    update: add one informational comment to route.jsx

--- app/api/auth/[...nextauth]/route.jsx
+++ app/api/auth/[...nextauth]/route.jsx
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
 import User from "@models/user";
 
+// Next API Authenticated Route to create a new user using Google OAuth
 const handler = NextAuth({
     providers: [
         GoogleProvider({
