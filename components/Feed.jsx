a5c13c27930248c4034a046c42d27e815fd79338
gtopinio <mctopinio@up.edu.ph>
2023-07-30 00:28:24 +0800

    update: add seach bar to feed page

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from "react"
+
+import PasswordCard from "@components/PasswordCard";
+
+const PasswordCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 passwordLayout">
+
+    </div>
+  );
+}
+
 
 const Feed = () => {
+
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e) => {
+  }
+
   return (
-    <div className='text-center'>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex justify-center">
+          <input
+            type="text"
+            className="searchInput"
+            placeholder="Search for a Code Vault password..."
+            value={searchText}
+            onChange={(e) => {
+              handleSearchChange
+            }}
+            required
+          >
+          </input>
+      </form>
+
+      <PasswordCardList
+          data={[]}
+          handleTagClick={()=>{}}
+      />
+    </section>
   )
 }
 
