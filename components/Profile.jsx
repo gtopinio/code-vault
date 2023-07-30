ed2bf2c7fc57960cd74f9d0e9672ac43e488a3b3
gtopinio <mctopinio@up.edu.ph>
2023-07-30 21:08:55 +0800

    update: implement profile component

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import PasswordCard from "./PasswordCard"
 
-const Profile = () => {
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
-    <div>Profile</div>
+    <section className="w-full">
+      <h1 className="headerText text-left">
+        <span className="indigoGradient">{name} Profile</span>
+      </h1>
+      <p className="descText text-left">{desc}</p>
+
+      <div className="mt-10 passwordLayout max-md:flex max-md:flex-col max-md:justify-center max-md:items-center ">
+      {data.map((password) => (
+        <PasswordCard
+          key={password._id}
+          password={password}
+          handleEdit={handleEdit && handleEdit(password)}  
+          handleDelete={handleDelete && handleDelete(password)}     
+        />
+      ))}
+    </div>
+
+    </section>
   )
 }
 
