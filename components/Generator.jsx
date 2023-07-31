879128d0a69aa1ed9d5ce9ac1127c567c062c2f2
gtopinio <mctopinio@up.edu.ph>
2023-07-31 14:40:11 +0800

    update: add session validator in generator component, add handlers

--- components/Generator.jsx
+++ components/Generator.jsx
@@ -2,10 +2,18 @@
 import { Password } from 'primereact/password';
 import { AutoComplete } from 'primereact/autocomplete';
 import { Dropdown } from 'primereact/dropdown';
+import { Toast } from 'primereact/toast';
 import { useState } from "react";
 import { generateRandomPassword } from "@utils/generate-pw";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useRef } from 'react';
 
 const Generator = () => {
+
+    const router = useRouter();
+    const { data: session } = useSession();
+    const copyToast = useRef(null);
 
     const [password, setPassword] = useState({
         value: generateRandomPassword(),
@@ -53,6 +61,43 @@ const Generator = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setSubmitting(true);
+
+        if(!session?.user.id || !session?.user._keyArray || !session?.user._ivArray){
+            console.log('User not logged in');
+            setSubmitting(false);
+            return;
+        }
+
+        try {
+            const response = await fetch('/api/password/new',
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    userId: session?.user.id,
+                    password: password.password,
+                    serviceName: password.serviceName,
+                    category: password.category,
+                    key: session?.user._keyArray,
+                    iv: session?.user._ivArray,
+                }),
+            });
+
+            if(response){
+                router.push('/profile');
+            }
+
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setSubmitting(false);
+        }
+
+    }
+
+    const handleCopyClick = () => {
+        navigator.clipboard.writeText(password.value);
+
+        copyToast.current.show({severity:'success', summary: 'Copied!', detail: 'Password copied to clipboard', life: 1500});
     }
 
     return (
@@ -79,68 +124,88 @@ const Generator = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col ml-2">
-                    <span className="font-semibold text-white text-xl mt-6">
-                        Which platform needs this password?
-                    </span>
-                    <span className="p-float-label mt-5">
-                    <AutoComplete id="serviceName"
-                    suggestions={serviceSuggestions}
-                    value={password.serviceName}
-                    completeMethod={searchService}
-                    required
-                    onChange={(e) => {
-                        setPassword({
-                            ...password,
-                            serviceName: e.target.value,
-                        })
-                    }} />
-                    <label htmlFor="serviceName">Service Name</label>
-                    </span>
-                </div>
+                {
+                    session?.user.id ? (
+                        <div>
+                            <div className="flex flex-col ml-2">
+                                <span className="font-semibold text-white text-xl mt-6">
+                                    Which platform needs this password?
+                                </span>
+                                <span className="p-float-label mt-5">
+                                <AutoComplete id="serviceName"
+                                suggestions={serviceSuggestions}
+                                value={password.serviceName}
+                                completeMethod={searchService}
+                                required
+                                onChange={(e) => {
+                                    setPassword({
+                                        ...password,
+                                        serviceName: e.target.value,
+                                    })
+                                }} />
+                                <label htmlFor="serviceName">Service Name</label>
+                                </span>
+                            </div>
 
-                <div className="flex flex-col ml-2">
-                    <span className="font-semibold text-white text-xl mt-6">
-                        Pick a Category
-                    </span>
-                    <span className="p-float-label mt-2">
-                    <Dropdown
-                        value={password.category}
-                        className="w-full"
-                        options={serviceCategories}
-                        required
-                        onChange={(e) => {
-                            setPassword({
-                                ...password,
-                                category: e.target.value,
-                                });
-                    }} />
-                    </span>
-                </div>
+                            <div className="flex flex-col ml-2">
+                                <span className="font-semibold text-white text-xl mt-6">
+                                    Pick a Category
+                                </span>
+                                <span className="p-float-label mt-2">
+                                <Dropdown
+                                    value={password.category}
+                                    className="w-full"
+                                    options={serviceCategories}
+                                    required
+                                    onChange={(e) => {
+                                        setPassword({
+                                            ...password,
+                                            category: e.target.value,
+                                            });
+                                }} />
+                                </span>
+                            </div>
+                        </div>
+                    ) : (
+                        <></>
+                    )
+                }
+                
 
-                <div className="flex justify-end mx-3 mb-1.5 gap-4 mt-7">
-                    <button type="button" className='cancelBtn' onClick={
-                        () => {
-                            setPassword({
-                                value: '',
-                                serviceName: '',
-                                category: '',
-                            })
-                        }
-                    }>
-                        Clear
-                    </button>
+                {
+                    session?.user.id ? (
+                        <div className="flex justify-end mx-3 mb-1.5 gap-4 mt-7">
+                            <button type="button" className='cancelBtn' onClick={
+                                () => {
+                                    setPassword({
+                                        value: '',
+                                        serviceName: '',
+                                        category: '',
+                                    })
+                                }
+                            }>
+                                Clear
+                            </button>
 
-                    <button
-                        type="submit"
-                        className="submitButton"
-                        disabled={submitting}
-                    >
-                        Save Password
-                    </button>
-                </div>
+                            <button
+                                type="submit"
+                                className="submitButton"
+                                disabled={submitting}
+                            >
+                                Save Password
+                            </button>
+                        </div>
+                    ):(
+                        <div className="flex justify-center mx-3 mb-1.5 gap-4 mt-7">
+                            <button  type="button" className='submitButton' onClick={handleCopyClick}>
+                                Copy Password
+                            </button>
+                        </div>
+                    )
+                }
 
             </form>
+            <Toast ref={copyToast} /> 
         </section>
     )
 }
